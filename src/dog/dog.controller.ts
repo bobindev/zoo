e2df84e7fc77f6433c7baa0ef35ec907a14a3654
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-05-06 22:31:42 +0900

    feat: Initial setup & create post api request

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { DogService } from './dog.service';
+
+@Controller('dog')
+export class DogController {
+  constructor(private readonly dogService: DogService) {}
+
+  @Get()
+  public getHello(): string{
+    return this.dogService.getHello();
+  }
+
+  @Get('greet/:id')
+  public introduce(@Param('id') id: string, @Query() query: any): string {
+    console.log('id:', id);
+    console.log('query:', query);
+    return this.dogService.introduce();
+  }
+
+  @Post('edit')
+  public modifyDetail(@Body() body:any): string {
+    console.log('body:', body);
+    return this.dogService.modifyDetail();
+  }
+}
